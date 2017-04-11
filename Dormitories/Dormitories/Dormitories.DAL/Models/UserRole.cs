2f003a00d4ecc472026f6afa040646c5e00ef9a1
Sem <andriy.semchyshyn@gmail.com>
2017-04-11 17:20:23 +0300

    rechange model UserRole

--- Dormitories/Dormitories/Dormitories.DAL/Models/UserRole.cs
+++ Dormitories/Dormitories/Dormitories.DAL/Models/UserRole.cs
@@ -8,12 +8,15 @@ namespace Dormitories.DAL.Models
 {
     public class UserRole
     {
+        public int Id { get; set; }
+        public string name { get; set; }
         public ICollection<User> Users { get; set; }
         public UserRole()
         {
             Users = new List<User>();
+            Id = 1;
+            name = "first";
         }
-        public int Id { get; set; }
-        public string name { get; set; }
+        
     }
 }
